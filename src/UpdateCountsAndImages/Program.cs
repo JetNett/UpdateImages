ef8dc14aac6dec66f89a07e4a8056d64fae57d2b
Kyle Gobel <kgobel@gmail.com>
2014-06-07 23:18:00 -0400

    initial file push

---
+++ src/UpdateCountsAndImages/Program.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack;
+
+namespace UpdateCountsAndImages
+{
+    class Program
+    {
+        static void Main()
+        {
+            "http://screenshot.jetnett.com/start".GetStringFromUrlAsync();
+        }
+    }
+}
